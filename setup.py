93448c0d87e82cacd60a539080126dab5637e110
Jake Snell <jsnell@cs.toronto.edu>
2015-05-06 16:04:43 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pynn',
+      version='0.1',
+      description='A simple and flexible python neural network package',
+      author='Yujia Li',
+      packages=['pynn'])
